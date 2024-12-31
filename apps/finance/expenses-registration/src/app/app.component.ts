68834ad2b2d83fd0d48028844263111645636a55
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-31 17:36:42 +0000

    feat: integrate navbar component into app layout with dynamic items

--- apps/finance/expenses-registration/src/app/app.component.ts
+++ apps/finance/expenses-registration/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '@angular-monorepo/navbar';
+import { NavbarItem } from '@angular-monorepo/common-components/model/navbar.model';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'expenses-registration';
+
+  navbarItems: NavbarItem[] = [
+    { label: 'expenses approval', route: '/expenses-approval' },
+  ];
 }
