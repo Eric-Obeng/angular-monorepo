d31762db0a8e3e9bc6af3383958053032759d321
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-31 17:36:26 +0000

    feat: add navbar library with configuration, routing, and testing setup

---
+++ libs/shared/ui/common-components/src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
+export const navbarRoutes: Route[] = [{ path: '', component: NavbarComponent }];
