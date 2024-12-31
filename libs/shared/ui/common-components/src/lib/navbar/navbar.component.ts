a1d5c41c47e396cf98cdf0f0528bec842c4dc16e
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-31 17:35:33 +0000

    feat: add navbar component with template, styles, and tests

---
+++ libs/shared/ui/common-components/src/lib/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NavbarItem } from '../model/navbar.model';
+
+@Component({
+  selector: 'lib-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent {
+  private _navItems: NavbarItem[] = [];
+
+  @Input()
+  set navbarItems(value: NavbarItem[]) {
+    this._navItems = [{ label: 'home', route: '/' }, ...value];
+  }
+
+  get navbarItems(): NavbarItem[] {
+    return this._navItems;
+  }
+}
