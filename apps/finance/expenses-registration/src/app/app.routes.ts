a2a80b97adefe358f3cfc6952db42da06ddff6a8
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-31 16:21:05 +0000

    feat: update routing to include title resolver and add redirects for navigation

--- apps/finance/expenses-registration/src/app/app.routes.ts
+++ apps/finance/expenses-registration/src/app/app.routes.ts
@@ -1,14 +1,31 @@
 import { Route } from '@angular/router';
-import { ExpensesOverviewPageComponent } from './pages/expenses-overview-page/expenses-overview-page.component';
-import { ExpensesApprovalPageComponent } from './pages/expenses-approval-page/expenses-approval-page.component';
+import { titleResolver } from './resolvers/title.resolver';
 
 export const appRoutes: Route[] = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'expenses-overview',
+  },
+  {
     path: 'expenses-overview',
-    component: ExpensesOverviewPageComponent,
+    loadComponent: () =>
+      import(
+        './pages/expenses-overview-page/expenses-overview-page.component'
+      ).then((m) => m.ExpensesOverviewPageComponent),
+    title: titleResolver,
   },
   {
     path: 'expenses-approval',
-    component: ExpensesApprovalPageComponent,
+    loadComponent: () =>
+      import(
+        './pages/expenses-approval-page/expenses-approval-page.component'
+      ).then((m) => m.ExpensesApprovalPageComponent),
+    title: titleResolver,
+  },
+  {
+    path: '**',
+    redirectTo: '/expenses-overview',
+    pathMatch: 'full',
   },
 ];
